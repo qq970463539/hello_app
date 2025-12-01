123826dce12c47d300e9eda95b16af188c56c8d8
qq970463539 <qq15609715576@gmail.com>
2025-12-02 08:03:49 +0900

    Finish layout and routes

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,8 @@
+test "layout links" do
+  get root_path
+  assert_template "static_pages/home"
+  assert_select "a[href=?]", root_path,    count: 2  # logo + Home
+  assert_select "a[href=?]", help_path
+  assert_select "a[href=?]", about_path
+  assert_select "a[href=?]", contact_path
+end
